a9a437842761750d55c4b201f87b12aebe77f8b7
Bayu Wicaksono <bwbayuuu@gmail.com>
2025-09-23 23:10:38 +0700

    feat add cors middleware on server side

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 # src/main.py
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
+from src.middleware.require_login import RequireLoginMiddleware
 from src.database.init import engine, init_db, SessionLocal
 from src.database.models import Base
 from src.api import auth
-from src.middleware.require_login import RequireLoginMiddleware
 
 # lifespan context manager: open and close db connection
 @asynccontextmanager
@@ -26,6 +27,13 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 # Middleware
 app.add_middleware(RequireLoginMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routers
 app.include_router(auth.router, prefix="/auth")
